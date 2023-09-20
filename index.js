ee1c905f69904c05a02db6519372ce9f6a1a97b7
Aditya <adityakrjha111@gmai.com>
2023-09-21 02:13:36 +0530

    Update

--- index.js
+++ index.js
@@ -1,24 +1,60 @@
-let tablinks = document.getElementsByClassName("tab-links");
-let tabcontents = document.getElementsByClassName("tab-contents");
-
-function opentab(tabname){
-  for(tablink of tablinks){
-    tablink.classList.remove("active-link");
+// Shadow for nav after scroll----------------
+let oldScroll = window.scrollY;
+window.addEventListener("scroll", () => {
+  let newScroll = window.scrollY;
+  if (oldScroll < newScroll) {
+    document.querySelector(".nav").classList.add("shadow");
+  } else {
+    document.querySelector(".nav").classList.remove("shadow");
   }
-  for(tabcontent of tabcontents){
-    tabcontent.classList.remove("active-tab");
+});
+
+
+// Toggle dark theme----------------------
+const themeToggle = document.querySelector(".checkbox");
+themeToggle.addEventListener("click", () => {
+  if(themeToggle.checked){
+    document.body.classList.add("dark-theme");
   }
-  event.currentTarget.classList.add("active-link");
-  document.getElementById(tabname).classList.add("active-tab");
-}
+  else{
+    document.body.classList.remove("dark-theme");
+  }
+})
 
+// Handling menu button and close button for nav----------
+const menuElement = document.querySelector(".menu-btn");
+const closeElement = document.querySelector(".close-btn");
 
-let sidemenu2 = document.getElementById("sidemenu");
+let visible = false;
+menuElement.addEventListener("click", () => {
+  if(!visible){
+    document.querySelector("#sidemenu").style.top = "70px"; 
+    visible = true;
+  }
+  else{
+    document.querySelector("#sidemenu").style.top = "-250px";
+    visible = false;
+  }
+    
+    
+  // closeElement.style.display = "inline-block";
+  // menuElement.style.display = "none";
+});
 
-function openmenu(){
-  sidemenu2.style.right = "0";
-}
+// closeElement.addEventListener("click", () => {
+//   document.querySelector("#sidemenu").style.top = "-250px";
+//   // menuElement.style.display = "inline-block";
+//   // closeElement.style.display = "none";
+// });
 
-function closemenu(){
-  sidemenu2.style.right = "-200px";
-}
+// menuElement.addEventListener("click", () => {
+//   document.querySelector("#sidemenu").style.display = "block";
+//   menuElement.style.display = "none";
+//   closeElement.style.display = "inline-block";
+// });
+
+// closeElement.addEventListener("click", () => {
+//   document.querySelector("#sidemenu").style.display = "none";
+//   menuElement.style.display = "inline-block";
+//   closeElement.style.display = "none";
+// });
